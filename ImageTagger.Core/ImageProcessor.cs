67268af586164afccb55cb0f75591e542730f465
Hyperledge <107569457+Hyperledge@users.noreply.github.com>
2022-08-24 16:00:35 +0000

    Position change.

--- ImageTagger.Core/ImageProcessor.cs
+++ ImageTagger.Core/ImageProcessor.cs
@@ -12,4 +12,5 @@ public class ImageProcessor
 
 
     /// <summary>
-    ///     Given an image it applies ne
+    ///     Given an image it applies necessary transformations and returns a tensor.
+    /// <
