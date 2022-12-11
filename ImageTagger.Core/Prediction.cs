5b6ffe068b6a16098789e790331aa948eaef9bf9
Hyperledge <107569457+Hyperledge@users.noreply.github.com>
2022-12-11 10:23:09 +0000

    can: c_can: Fix RX message handling, handle lost message before EOB

--- ImageTagger.Core/Prediction.cs
+++ ImageTagger.Core/Prediction.cs
@@ -4,4 +4,4 @@
 ///     Prediction is a record that represents a prediction made by a model.
 /// </summary>
 /// <param name="Label">The prediction label / category.</param>
-/// <param name="Confidence">The confidenc
+/// <param name="Confidence">The confidence level of the prediction. Higher is better.</param>
