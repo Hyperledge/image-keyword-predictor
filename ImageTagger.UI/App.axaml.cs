93d13ba7e464ff26c1d2b91bd93380cd1487f3d4
Hyperledge <107569457+Hyperledge@users.noreply.github.com>
2023-06-15 22:04:25 +0000

    Naming.

--- ImageTagger.UI/App.axaml.cs
+++ ImageTagger.UI/App.axaml.cs
@@ -2,4 +2,5 @@ using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using ImageTagger.UI.Servi
+using ImageTagger.UI.Service;
+using ImageTagger.UI.Views.MainWindow;
