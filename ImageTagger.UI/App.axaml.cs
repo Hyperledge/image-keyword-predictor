53967823e94826e3efccc4965784f195a9902b70
Hyperledge <107569457+Hyperledge@users.noreply.github.com>
2023-06-25 09:29:05 +0000

    fixed mobile

--- ImageTagger.UI/App.axaml.cs
+++ ImageTagger.UI/App.axaml.cs
@@ -4,3 +4,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using ImageTagger.UI.Service;
 using ImageTagger.UI.Views.MainWindow;
+
+namespace ImageTagger.UI;
+
+/// <summary>
