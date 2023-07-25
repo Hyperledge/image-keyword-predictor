e1be5532a5f5a5c6728057647acb5cb830aafb2e
Hyperledge <107569457+Hyperledge@users.noreply.github.com>
2023-07-25 17:19:39 +0000

    no sudo

--- ImageTagger.UI/Controls/ImagePredictionRow.axaml.cs
+++ ImageTagger.UI/Controls/ImagePredictionRow.axaml.cs
@@ -4,4 +4,5 @@ using Avalonia.Controls.Primitives;
 using Avalonia.Media.Imaging;
 using Avalonia.Metadata;
 using SixLabors.ImageSharp;
-using SixLabors.I
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSha
