aabc360a12b93e425e32ef46ab7c7f8656cda62b
Hyperledge <107569457+Hyperledge@users.noreply.github.com>
2023-10-26 21:48:03 +0000

    docblock fixes

--- ImageTagger.UI/Controls/ImagePredictionRow.axaml.cs
+++ ImageTagger.UI/Controls/ImagePredictionRow.axaml.cs
@@ -88,4 +88,4 @@ public class ImagePredictionRow : TemplatedControl
     {
         // Resize the image to 224x224 and save it into a temporary file.
         var temporaryFilePath = Path.GetTempFileName();
-        
+        var image = SixLabors.ImageSharp.Image.Load<Rgb24>(_
