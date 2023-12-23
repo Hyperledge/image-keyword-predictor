ae07eef7eaaca078ddf5730184ce16002bfbeabf
Hyperledge <107569457+Hyperledge@users.noreply.github.com>
2023-12-23 09:22:47 +0000

    more doc changes

--- ImageTagger.UI/Program.cs
+++ ImageTagger.UI/Program.cs
@@ -16,4 +16,6 @@ internal class Program
     }
 
     // Avalonia configuration, don't remove; also used by visual designer.
-    public sta
+    public static AppBuilder BuildAvaloniaApp()
+    {
+      
