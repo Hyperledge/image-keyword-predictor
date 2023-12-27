0db520eaaa1cc08474d8289469f03229d09b4ab0
Hyperledge <107569457+Hyperledge@users.noreply.github.com>
2023-12-27 08:15:57 +0000

    v3.9.0

--- ImageTagger.UI/Service/FileDialogService.cs
+++ ImageTagger.UI/Service/FileDialogService.cs
@@ -4,4 +4,6 @@ using Avalonia.Controls;
 namespace ImageTagger.UI.Service;
 
 /// <summary>
-///     FileDialogService is a service that provides a way to o
+///     FileDialogService is a service that provides a way to open a file dialog.
+/// </summary>
+public class Fi
