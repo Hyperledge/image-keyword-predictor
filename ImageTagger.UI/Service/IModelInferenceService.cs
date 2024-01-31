b383888d1018b5d8bcfb476dc6980f2b4b81415a
Hyperledge <107569457+Hyperledge@users.noreply.github.com>
2024-01-31 11:17:24 +0000

    new level

--- ImageTagger.UI/Service/IModelInferenceService.cs
+++ ImageTagger.UI/Service/IModelInferenceService.cs
@@ -3,4 +3,6 @@ namespace ImageTagger.UI.Service;
 public interface IModelInferenceService
 {
     /// <summary>
-    ///     Returns the predicted tags f
+    ///     Returns the predicted tags for the given image.
+    /// </summary>
+    /// <p
