8dbf0d723943e88080bd229b75b76c18e216ccf2
Hyperledge <107569457+Hyperledge@users.noreply.github.com>
2024-02-01 06:39:54 +0000

    Removing whitespace

--- ImageTagger.UI/Service/IModelInferenceService.cs
+++ ImageTagger.UI/Service/IModelInferenceService.cs
@@ -5,4 +5,5 @@ public interface IModelInferenceService
     /// <summary>
     ///     Returns the predicted tags for the given image.
     /// </summary>
-    /// <param name="imagePath">The absolute path to 
+    /// <param name="imagePath">The absolute path to the image for predicting the tags.</param>
+    /
