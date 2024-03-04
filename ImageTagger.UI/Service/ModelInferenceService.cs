a8c41483ecae36098e2c6ab08f794286fee26236
Hyperledge <107569457+Hyperledge@users.noreply.github.com>
2024-03-04 07:04:19 +0000

    fixing merge error

--- ImageTagger.UI/Service/ModelInferenceService.cs
+++ ImageTagger.UI/Service/ModelInferenceService.cs
@@ -20,4 +20,5 @@ public class ModelInferenceService : IModelInferenceService
     public ModelInferenceService()
     {
         // Get assembly base path
-        var assemblyBasePath = Assembly.GetExecutingAssemb
+        var assemblyBasePath = Assembly.GetExecutingAssembly().Location;
+        var assemblyPath = Path.GetD
