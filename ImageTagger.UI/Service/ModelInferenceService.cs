bcfa6fc28c9aa44f4882a7462dd3f400c6bf79bc
Hyperledge <107569457+Hyperledge@users.noreply.github.com>
2024-03-12 22:31:56 +0000

    Update blackjack.js

--- ImageTagger.UI/Service/ModelInferenceService.cs
+++ ImageTagger.UI/Service/ModelInferenceService.cs
@@ -32,4 +32,5 @@ public class ModelInferenceService : IModelInferenceService
     ///     Returns the predicted tags for the given image.
     /// </summary>
     /// <param name="imagePath">The absolute path to the image for predicting the tags.</param>
-    /// <param name="separat
+    /// <param name="separator">The separator for the predicted tags</param>
+    //
